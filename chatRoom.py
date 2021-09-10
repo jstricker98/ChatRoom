920a1c6d2936c8ee1e34569171e21948d42ad42f
Jacob Stricker <50211425+jstricker98@users.noreply.github.com>
2021-09-10 09:16:32 -0400

    Added project files.
    The project was finished before hand so the initial commit is the final product.

---
+++ chatRoom.py
@@ -0,0 +1,214 @@
+import tkinter as tk
+import socket
+import threading
+
+window = tk.Tk()
+window.title("Sever")
+
+# Top frame consisting of two buttons widgets (i.e. btnStart, btnStop)
+topFrame = tk.Frame(window)
+btnStart = tk.Button(topFrame, text="Connect", command=lambda : start_server())
+btnStart.pack(side=tk.LEFT)
+btnStop = tk.Button(topFrame, text="Stop", command=lambda : stop_server(), state=tk.DISABLED)
+btnStop.pack(side=tk.LEFT)
+topFrame.pack(side=tk.TOP, pady=(5, 0))
+
+# Middle frame consisting of two labels for displaying the host and port info
+middleFrame = tk.Frame(window)
+lblHost = tk.Label(middleFrame, text = "Host: X.X.X.X")
+lblHost.pack(side=tk.LEFT)
+lblPort = tk.Label(middleFrame, text = "Port:XXXX")
+lblPort.pack(side=tk.LEFT)
+middleFrame.pack(side=tk.TOP, pady=(5, 0))
+
+# The client frame shows the client area
+clientFrame = tk.Frame(window)
+lblLine = tk.Label(clientFrame, text="**********Client List**********").pack()
+scrollBar = tk.Scrollbar(clientFrame)
+scrollBar.pack(side=tk.RIGHT, fill=tk.Y)
+tkDisplay = tk.Text(clientFrame, height=15, width=30)
+tkDisplay.pack(side=tk.LEFT, fill=tk.Y, padx=(5, 0))
+scrollBar.config(command=tkDisplay.yview)
+tkDisplay.config(yscrollcommand=scrollBar.set, background="#F4F6F7", highlightbackground="grey", state="disabled")
+clientFrame.pack(side=tk.BOTTOM, pady=(5, 10))
+
+
+server = None
+HOST_ADDR = "127.0.0.1"
+HOST_PORT = 8080
+client_name = " "
+clients = []
+clients_names = []
+
+
+# Start server function
+def start_server():
+    global server, HOST_ADDR, HOST_PORT # code is fine without this
+    btnStart.config(state=tk.DISABLED)
+    btnStop.config(state=tk.NORMAL)
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print (socket.AF_INET)
+    print (socket.SOCK_STREAM)
+
+    server.bind((HOST_ADDR, HOST_PORT))
+    server.listen(5)  # server is listening for client connection
+
+    threading._start_new_thread(accept_clients, (server, " "))
+
+    lblHost["text"] = "Host: " + HOST_ADDR
+    lblPort["text"] = "Port: " + str(HOST_PORT)
+
+    #start chat client
+    exec(open('Chatter.py').read())
+
+# Stop server function
+def stop_server():
+    global server
+    btnStart.config(state=tk.NORMAL)
+    btnStop.config(state=tk.DISABLED)
+
+
+def accept_clients(the_server, y):
+    while True:
+        client, addr = the_server.accept()
+        clients.append(client)
+        threading._start_new_thread(send_receive_client_message, (client, addr))
+
+
+# Function to receive message from current client AND
+# Send that message to other clients
+def send_receive_client_message(client_connection, client_ip_addr):
+    global server, client_name, clients, clients_addr
+    client_msg = " "
+
+    # send welcome message to client
+    client_name  = client_connection.recv(4096).decode()
+    client_connection.send(("Welcome " + client_name + ". Use 'exit' to quit").encode())
+
+    clients_names.append(client_name)
+
+    update_client_names_display(clients_names)  # update client names display
+
+
+    while True:
+        data = client_connection.recv(4096)
+        if not data: break
+        if data == "exit": break
+
+        client_msg = data.decode()
+
+        idx = get_client_index(clients, client_connection)
+        sending_client_name = clients_names[idx]
+
+        for c in clients:
+            if c != client_connection:
+                c.send((sending_client_name + "->" + client_msg).encode())
+
+    # find the client index then remove from both lists(client name list and connection list)
+    idx = get_client_index(clients, client_connection)
+    del clients_names[idx]
+    del clients[idx]
+    client_connection.send("BYE!")
+    client_connection.close()
+
+    update_client_names_display(clients_names)  # update client names display
+
+
+# Return the index of the current client in the list of clients
+def get_client_index(client_list, curr_client):
+    idx = 0
+    for conn in client_list:
+        if conn == curr_client:
+            break
+        idx = idx + 1
+
+    return idx
+
+
+# Update client name display when a new client connects OR
+# When a connected client disconnects
+def update_client_names_display(name_list):
+    tkDisplay.config(state=tk.NORMAL)
+    tkDisplay.delete('1.0', tk.END)
+
+    for c in name_list:
+        tkDisplay.insert(tk.END, c+"\n")
+    tkDisplay.config(state=tk.DISABLED)
+
+def connect():
+    global username, client
+    if len(entName.get()) < 1:
+        tk.messagebox.showerror(title="ERROR!!!", message="You MUST enter your first name <e.g. John>")
+    else:
+        username = entName.get()
+        connect_to_server(username)
+
+
+# network client
+entName = tk.Entry(topFrame)
+
+client = None
+HOST_ADDR = "127.0.0.1"
+HOST_PORT = 8080
+
+def connect_to_server(name):
+    global client, HOST_PORT, HOST_ADDR
+    try:
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect((HOST_ADDR, HOST_PORT))
+        client.send(name.encode())
+
+        entName.config(state=tk.DISABLED)
+
+        threading._start_new_thread(receive_message_from_server, (client, "m"))
+    except Exception as e:
+        tk.messagebox.showerror(title="ERROR!!!", message="Cannot connect to host: " + HOST_ADDR + " on port: " + str(HOST_PORT) + " Server may be Unavailable. Try again later")
+
+
+def receive_message_from_server(sck, m):
+    while True:
+        from_server = sck.recv(4096).decode()
+
+        if not from_server: break
+
+        texts = tkDisplay.get("1.0", tk.END).strip()
+        tkDisplay.config(state=tk.NORMAL)
+        if len(texts) < 1:
+            tkDisplay.insert(tk.END, from_server)
+        else:
+            tkDisplay.insert(tk.END, "\n\n"+ from_server)
+
+        tkDisplay.config(state=tk.DISABLED)
+        tkDisplay.see(tk.END)
+
+    sck.close()
+    window.destroy()
+
+def getChatMessage(msg):
+
+    msg = msg.replace('\n', '')
+    texts = tkDisplay.get("1.0", tk.END).strip()
+
+    tkDisplay.config(state=tk.NORMAL)
+    if len(texts) < 1:
+        tkDisplay.insert(tk.END, "You->" + msg, "tag_your_message") # no line
+    else:
+        tkDisplay.insert(tk.END, "\n\n" + "You->" + msg, "tag_your_message")
+
+    tkDisplay.config(state=tk.DISABLED)
+
+    send_mssage_to_server(msg)
+
+    tkDisplay.see(tk.END)
+
+
+def send_mssage_to_server(msg):
+    client.send(msg.encode())
+    if msg == "exit":
+        client.close()
+        window.destroy()
+    print("Sending message")
+
+
+window.mainloop()
